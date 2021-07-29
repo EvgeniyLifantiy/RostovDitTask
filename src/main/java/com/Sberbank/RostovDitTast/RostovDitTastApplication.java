3e9ed69b9f39b369de845468b5786e842e8d16fc
Evgeniy <lifantii@inbox.ru>
2021-07-29 14:56:18 +0300

    first commit

---
+++ src/main/java/com/Sberbank/RostovDitTast/RostovDitTastApplication.java
@@ -0,0 +1,13 @@
+package com.Sberbank.RostovDitTast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RostovDitTastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RostovDitTastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Sberbank/RostovDitTast/RostovDitTastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sberbank.RostovDitTast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RostovDitTastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
